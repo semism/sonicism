870cddd8bc2a03d6413b4b86b9630e3eb7f9637f
_semism_ <128905611+semism@users.noreply.github.com>
2025-09-12 12:53:51 +0200

    Update v2_doomer_clanker.js

--- strudel/chilllcoustic/v2_doomer_clanker.js
+++ strudel/chilllcoustic/v2_doomer_clanker.js
@@ -23,15 +23,15 @@ arpeg:n("0 2 4 <7!3 ->").scale("<C:minor C:minor D:major D:minor>").delay(.5)
 .clip(2).s("gm_electric_guitar_clean, gm_overdriven_guitar").lpf(800)
 
 
-base:n("<0>").scale("<C2:minor C2:minor D2:major D2:minor>")
-.clip(2).s("gm_electric_guitar_clean").room(4).roomsize(4).gain(.3)
+// base:n("<0>").scale("<C2:minor C2:minor D2:major D2:minor>")
+// .clip(2).s("gm_electric_guitar_clean").room(4).roomsize(4).gain(.3)
 
 kick:s("bd").beat("0, 2, 3, 4",8).room(.4)
 snare:s("sd").beat("1, 3", 4).room(.4).gain(.4)
 rim: s("<rim misc>").beat("4, 16, 18",32).room(.4).gain(.4)
   .almostNever(ply("2 | 4"))
-beeper: note("c7").beat("5, 17, 31", 32).s("sine").decay(.05)
-  .jux(rev).delay(.5)
+// beeper: note("c7").beat("5, 17, 31", 32).s("sine").decay(.05)
+//   .jux(rev).delay(.5)
 hihat:s("white!8").decay(.1).sometimesBy(.15, x=>x.decay(.7))
   .gain(.4).pan(1)
 
@@ -49,4 +49,4 @@ sub:n("<0>!4").clip(1)
   .scale("<C2:minor C2:minor D2:major D2:minor>")
   .s("sine, supersaw").lpf(2000).lpenv(2).lpq(6).room(2)
 
-
+all(x=>x.almostNever(ply(4)))
