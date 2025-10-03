ded6d1e8730e4e3a260f0aa9147d365537ad46db
_semism_ <128905611+semism@users.noreply.github.com>
2025-10-03 14:14:18 +0200

    Update candybeat.js

--- strudel/candybeat.js
+++ strudel/candybeat.js
@@ -1,14 +1,14 @@
 setcpm(140/4)
-$: s("[bd <bd, sd, cp> <bd [bd, hh*2]> [bd, sd,<cp ->, hh*<2 4 8>]]").bank("tr707")
 
-$: n("4@2 5 6 4 5")
+kick: s("sbd!4").duck("2:3:4")
+snare: s("sd!2")
+hat:s("white!16").clip(.2).sometimesBy(.5, x=>x.clip(.7)).gain(.2).orbit(2)
+
+bass_guitar: n("4@2 5 6 4 5")
   .sound("<gm_electric_guitar_clean@8 <gm_electric_guitar_muted gm_distortion_guitar>>")
   .scale("<D1:minor C1:minor>")
+  .orbit(3)
 
-$: n("[4!2 - 4] <[5 5] [5 <[3 <1 5>] 5>]> ")
+tune: n("[4!2 - 4] <[5 5] [5 <[3 <1 5>] 5>]> ")
   .sound("sine")
   .scale("<<D4:major , D1:major> C4:dorian>")
-
-$: note("a2@3 <a4 a4 a5*4.3>")
-  .sound("sawtooth")
-  .lpf("<1500 2000 500>")
