5b59db261ecb6d65f54cf72edfce192759a1aa13
_semism_ <128905611+semism@users.noreply.github.com>
2025-11-02 23:21:38 +0100

    Update candybeat.js

--- strudel/candybeat.js
+++ strudel/candybeat.js
@@ -4,11 +4,6 @@ kick: s("sbd!4").duck("2:3:4")
 snare: s("sd!2")
 hat:s("white!16").clip(.2).sometimesBy(.5, x=>x.clip(.7)).gain(.2).orbit(2)
 
-bass_guitar: n("4@2 5 6 4 5")
-  .sound("<gm_electric_guitar_clean@8 <gm_electric_guitar_muted gm_distortion_guitar>>")
-  .scale("<D1:minor C1:minor>")
-  .orbit(3)
-
 tune: n("[4!2 - 4] <[5 5] [5 <[3 <1 5>] 5>]> ")
   .sound("sine")
   .scale("<<D4:major , D1:major> C4:dorian>")
