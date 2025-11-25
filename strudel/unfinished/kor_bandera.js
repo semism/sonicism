30cddb0d64804ef18384f3aaf7546b180bb79ac6
_semism_ <128905611+semism@users.noreply.github.com>
2025-11-26 00:33:33 +0100

    Update kor_bandera.js

--- strudel/unfinished/kor_bandera.js
+++ strudel/unfinished/kor_bandera.js
@@ -7,7 +7,7 @@ const scales = "<e:minor [c:major d:major] g:major d:major>/4";
 const kit = "akaimpc60"
 
 kick: s("bd!4").bank(kit).duck("2:3")
-snare: s("sd!2").struct("- x - x").bank(kit).ply("1 2").lpf(2000)
+snare: s("sd!2").struct("- x - x").bank(kit).ply("1 2")
 hat: s("hh").struct("x x - x x x - x x x x - x x x x").bank(kit).gain(rand.rib(44,3))
 ophat: s("oh").struct("x - x - - - x - - - - x - - x -").bank(kit).delay(.5).gain(".5 .8")
 
@@ -29,7 +29,7 @@ bass: n("0!16".add(-7).add("<0 3 4 5>")).scale(scales).s("supersaw")
 
 
 lead2: n(irand(6).seg(8).add("7").add("<0 3 4 5>").rib("<99 5 32 30>", 4)).scale(scales)
-  .s("saw").distort("4").pan(sine.slow(16))
+  .s("supersaw").distort("2").pan(sine.slow(16))
   .delay(".2,.5")
   .crush(6).lpf(1400).lpenv(3).lpq(10)
   ._pianoroll()
