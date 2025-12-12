65fac3b57786200b7513e2b246bf541031e54227
_semism_ <128905611+semism@users.noreply.github.com>
2025-12-12 21:24:49 +0100

    Update not_trynna_strike_a_chord.js

--- strudel/unfinished/not_trynna_strike_a_chord.js
+++ strudel/unfinished/not_trynna_strike_a_chord.js
@@ -1,17 +1,19 @@
-samples('github:semism/nibbles')
 
 setcpm(120/4) 
 
-kick: s("bd:3!4").gain(1.6)
+kick: s("bd:3!4").gain(1.6).duck("2:3")
+  .duckattack("<.2 0 .2 0>*2")
 snare: s("sd:2")
-hat: s("hh!4")
+hat: s("hh!8")
 rim: s("rim:1").struct(rand.round().rib(4, 2).seg(8))
 
 line: n("[0!2] 0 0 [3!2] 4 0 0 4".add("0, -7"))
   .scale("a:minor").s("saw").delay(".2, .5").gain(.7)
-  .delay(.2).lpf(saw.range(3000, 6000)).lpenv(4)
+  .delay(.2).lpf(saw.range(3000, 6000)).lpa(.4).lpenv(4)
   .room(1).size(6)
   .every("4", x=>x.clip(".5 .7").ply(2))
 
-bass: n("0!16".add(-7)).scale("a:minor").s("supersaw")
-._pianoroll()
+bass: n("0!16".add(-7)).scale("a:minor").s("saw").room(2).size(15).vib("5")
+  .distort("3:.2")
+  .orbit(2)
+._scope()
