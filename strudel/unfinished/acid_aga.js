d15c85be49d22214baebd9777cd5c7e0078beb36
_semism_ <128905611+semism@users.noreply.github.com>
2025-12-17 21:26:52 +0100

    Update acid_aga.js

--- strudel/unfinished/acid_aga.js
+++ strudel/unfinished/acid_aga.js
@@ -7,31 +7,29 @@ let acid_env_mod = sine.range(2, 3).slow(32)
 let acid_res_mod = sine.range(12, 21).slow(8);
 let chords = ["<e:minor a:minor e:minor a:minor g:minor d:minor>", "a:minor"];
 
-
-let acid_bass = n("{-7 0!2 2 7 0 0 - 2 4 -2!6}".add("-7"))
+let acid_bass = n("{-7 0@2 2 7 0 0 2}*2".add("-7"))
   .scale(pick(chords, 1)).s("saw").att(sine.range(0.02, .1))
   .decay("{.4@5 2@2 .4}*2").velocity(itri.range(.7, 1).slow(2))
   .lpf(acid_lpf_mod).lpq(acid_res_mod)
   .lpenv(acid_env_mod)
   .distort("2.2:.4")
 
-acid_layer_main: acid_bass
+acid_layer_main: acid_bass.room(.5).size(6)
 acid_layer_sub: acid_bass  
   .off(1/4, x=>x.s("supersaw")).velocity(.35)
 
 bass: n("{-7 0@2 2 7 0 0 2}*2".add("-7"))
-  .scale("<a:minor>").s("sine").att(sine.range(0.02, .1))
+  .scale("<a:minor>").s("sine")
+  .att(sine.range(0.02, .1))
 
-let kick = s("bd bd:1").room(.3).duck(2)
-  .every(4, x=>x.ply(2 | 4)).velocity(.8)
-let snare = s("sd".struct("0 1 0 1")).room(.3).delay(.5).orbit(2)
+let kick = s("bd!4")
+  // .decay(0.05)
+let snare = s("sd".struct("0 1 0 1")).room(.3).delay(.25).delaytime(.04)
 let clap = s("cp".struct("0 0 0 1").late(.25)).velocity(.4)
-let crash = s("cr".struct("0 0 1*2 0")).velocity(1.5).clip(.2)
-let hat = s("hh")
-  .struct("0 1 0 1*2 0 1 0 1".early(.25))
+let hat = s("hh").clip(.2)
+  .struct("<0 1*2> 1 0 1*2 0 1 0 1".early(.25))
   .velocity(".4 .3 .8 .5")
 
-drums: stack(kick, snare, crash, hat).bank("tr606")
+drums: stack(kick, snare, clap, hat).bank("tr505").room(.3).size(6)
 
 // all(x=>x.hpf(70))
-
