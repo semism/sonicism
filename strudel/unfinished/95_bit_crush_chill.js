12342aa5029fdd44e21e96069c6a8dacd0ecf84d
_semism_ <128905611+semism@users.noreply.github.com>
2025-12-22 20:08:44 +0100

    Update 95_bit_crush_chill.js

--- strudel/unfinished/95_bit_crush_chill.js
+++ strudel/unfinished/95_bit_crush_chill.js
@@ -5,7 +5,7 @@ let chords = ["<e:minor a:minor e:minor g:minor>", "a:minor"];
 
 kick: s("bd").bank("tr707").room(1).size(12).velocity(.5)
   .struct("1 - - 1  - 1 - 1  - - - 1  - - 1 -")
-  .clip(.05).duck(2)
+  .clip(.05)
 
 hat: s("white")
   .clip(.2).room(1).size(12)
@@ -14,12 +14,12 @@ hat: s("white")
 
 boop: note("g4").s("pulse").beat("<16 ->", 16).echo(3, 1/16,.5)
 
-osicalate: n("0 1 3 5 6 2 4 2".add(0))
+osicalate: n("0 1 3 5 6 2 4 2".add("0"))
   .scale(pick(chords, 0))
   .s("gm_overdriven_guitar").ply(16)
-  .lpf(1350).crush(10).distort(1)
+  .lpf(1350).crush(10).distort(1.6)
 
-perc_bass: n("0".add(-14))
+perc_bass: n("0".add("-14"))
   .scale(pick(chords, 0)).s("supersaw")
   .struct("<{- - 1 -  - - 1 -  - - 1 -  - 1 - 1}/2 ->")
   .delay(.5).delaytime("0 .25 0 0")
