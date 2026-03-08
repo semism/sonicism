16cc5db8df2994206494e0a47ad8e9fd0976c2f3
_semism_ <128905611+semism@users.noreply.github.com>
2026-03-08 21:08:25 +0100

    Update big_auga.js

--- strudel/unfinished/big_auga.js
+++ strudel/unfinished/big_auga.js
@@ -21,3 +21,9 @@ amen0: s("breaks/2").fit().scrub("{0@2 0@2 4@4}%8".div(16)).pan(.7)
 amen1: s("breaks:1/2").fit().scrub("<1 2 5 2>".div(16))
 
 amen2: s("breaks:2/2").fit().scrub("{0@2 0@2 <3 4 3 [2 1]>@4}%8".div(16)).pan(.4)
+
+hihat: s("white!16").clip(.1).release(rand.div(4).rib(12, 2)).velocity(.5).pan(.3)
+rim: s("rim!16").velocity(rand.rib(12, 2).mul(1.4)).almostNever(ply(4)).pan(.8)
+
+// all(x=>x.lpf(100))
+// all(x=>x.lpf(sine.range(100, 300).fast(16)).distort("11:.5").postgain(.1))
