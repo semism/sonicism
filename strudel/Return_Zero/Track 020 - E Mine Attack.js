5d312de4e0448e4f115b428a99bdae9b5152d1f1
semism <semism>
2026-05-10 01:51:14 +0200

    reverted

--- strudel/Return_Zero/Track 020 - E Mine Attack.js
+++ strudel/Return_Zero/Track 020 - E Mine Attack.js
@@ -1,55 +1,41 @@
 samples('github:semism/smbreaks')
 setcpm(70/4)
 
-sing: n(irand(5).seg(4).rib("<142 <44 123>>/2", 2))
-  .scale("d3:minor")
-  .s("gm_voice_oohs:2")
-  .mask("<1 0>")
-  .room(4).att(1).rel(1)
-  .lpf(sine.range(100, 3000).fast(16))
-  ._spectrum()
+//in order
+dum: s("bd").beat("0, 8, 16, 24", 32).delay(.5).hpf(150)
+  .off(1/4, x=>x.s("lt").ply(2)).duck(2)
+  ._scope()
+tak: s("- - rim - - - rim - - - - - rim:2 - rim  - - - rim - - - rim - - - rim - rim - rim:2 -").hpf(3000).delay(.5)
+._scope()
+cb: s("pink").beat("0, 1, 6", 16).delay(".2, .5, .1").clip(.2)
+  .color("pink")._scope()
 
-water: n(irand(5).seg(4).rib("<142 <152!2 123 45>>/2", 2))
-  .scale("d3:minor")
-  .s("supersaw, gm_sitar")
-  .mask("<- 1> <1 ->")
-  .rel(.5).att(.1)
-  .lpf(50).lpenv(2).lpq(6)
-  .lpa(sine.range(.1, .5).fast(2))
-  .lpr(.5)
-  .delay(".25:.5:.5").room(1)
+amin: s("breaks:3/2, breaks:4/2").fit() //brk 3 alt 4
+
+bass: note("e2, e3!8").s("gm_electric_bass_finger:3")
+  .every(4, ply(2))
+  .gain(1.2)
+  .distort("4.5:.3")
+
+arpeg: n("<0, -7, <7 ->>!16".add(berlin.fast(2).mul(7).rib("<12!2 14 53>", 2))) //berlin saw perlin 1s =>16ths and
+  .scale("e:minor").rel(.01).attack(.3).s("tri,supersaw").gain(.6).pan(sine.slow(2))
   ._pianoroll()
 
-wt: s("basique").bank("wt_digital").seg(4).n("<0 2 0 ->").scale("d2:minor")
-  .warp("0.4 .5".slow(2))
-  .warpmode("asym")._spectrum()
+const melody = arrange(
+  [1, "0@2"],
+  [1, "1!2"],
+  [1, "0@2"],
+  [1, "4!2"],
+  [1, "[5 4] 2 3!2"],
+  [1, "4 5 6!2"],
+  [1, "[5 4] 9 [11 12] [[11 12]!3]"],
+  [1, "11 12 10 7 4 8!2 12"],
+  [1, "11 12 10 7 4 8!2 12 5 3 10 7 4 8!2 [4 5]"],
+  [1, "6 6 7 6"],
+  [1, "6 5 5 4"],
+  [1, "11 12 10 7 4 8!2 12 5 3 10 7 4 8!2 [4 5]"],
+)
 
-snare_bass_synth: n(irand(5).seg(8)
-              .rib(142, 2).add("-28")).scale("d1:minor").s("sd:2").ply(16).orbit(2).orbit(4)
-              .velocity(.07).delay(".5:.5:.5")
-  .distort(1)
-  ._pianoroll()
-
-
-nausea: n("<0 - 0 ->").scale("d1:minor").s("supersaw")
-  .att(.1).rel(.2)
-  .lpf(500).lpa(.3).lpe(tri.range(1, 2).slow(4))
-  .lpq(saw.range(6, 12).slow(2))
-  .hpf(200)
-
-// amen: s("breaks:4/2").fit().duck("2:3").duckdepth(.5)
-//   .scrub(irand(16).div(16).seg(8).rib(43, 1))
-//   .delay(".5, .1").gain(.3)
-//   .almostNever(ply("2 | 4"))
-//   ._scope()
-// floor: s("bd!4").duck("4")
-// .delay(".3:.25:.6").velocity(.7)
-
-
-
-ants: n(irand(5).seg(8).rib(142, 2)).scale("d3:minor").s("saw")
-  .delay(".5:.5:.5").hpf(1200).lpf(1700)
-  .clip(.05)
-
-
-
+sint: melody.n().s("supersaw, gm_distortion_guitar").scale("e4:minor")
+  .decay(3).room(2).delay(.2)
+  .cutoff(2000).hpf(400).fm(1).fmwave("white")
