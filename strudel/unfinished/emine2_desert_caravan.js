60d5c23d3015cefab2b217ebe3085e30e818b97a
_semism_ <128905611+semism@users.noreply.github.com>
2026-05-10 01:29:25 +0200

    Update emine2_desert_caravan.js

--- strudel/unfinished/emine2_desert_caravan.js
+++ strudel/unfinished/emine2_desert_caravan.js
@@ -1,34 +1,13 @@
 samples('github:semism/smbreaks')
 setcpm(70/4)
 
-nausea: n("<0!4 2!2 5!2>").scale("d1:minor").s("supersaw")
-  .att(.1).rel(.2)
-  .lpf(500).lpa(.3).lpe(tri.range(1, 2).slow(4))
-  .lpq(saw.range(6, 12).slow(2))
-  .hpf(200)
-
-amen: s("breaks:4/2").fit().duck("2:3").duckdepth(.5)
-  .scrub(irand(16).div(16).seg(8).rib(43, 1))
-  .delay(".5, .1").gain(.3)
-  .almostNever(ply("2 | 4"))
-  ._scope()
-floor: s("bd!4").duck("4")
-.delay(".3:.25:.6").velocity(.7)
-
-snare_bass_synth: n(irand(5).seg(8)
-              .rib(142, 2).add("-28")).scale("d1:minor").s("sd:2").ply(16).orbit(2).orbit(4)
-              .velocity(.07).delay(".5:.5:.5")
-  .distort(1)
-  ._pianoroll()
-
-
-synth: n(irand(5).seg(8).rib(142, 2)).scale("d3:minor").s("saw").orbit(3)
-  .delay(".5:.5:.5").hpf(1200).lpf(1700).lpa(.3).hpr(.3)
-  .att(.1).rel(.2)
-
-wt: s("basique").bank("wt_digital").seg(4).n("<0 2 0 ->").scale("d2:minor")
-  .warp("0.4 .5".slow(2))
-  .warpmode("asym")._spectrum()
+sing: n(irand(5).seg(4).rib("<142 <44 123>>/2", 2))
+  .scale("d3:minor")
+  .s("gm_voice_oohs:2")
+  .mask("<1 0>")
+  .room(4).att(1).rel(1)
+  .lpf(sine.range(100, 3000).fast(16))
+  ._spectrum()
 
 water: n(irand(5).seg(4).rib("<142 <152!2 123 45>>/2", 2))
   .scale("d3:minor")
@@ -41,10 +20,35 @@ water: n(irand(5).seg(4).rib("<142 <152!2 123 45>>/2", 2))
   .delay(".25:.5:.5").room(1)
   ._pianoroll()
 
-sing: n(irand(5).seg(4).rib("<142 <44 123>>/2", 2))
-  .scale("d3:minor")
-  .s("gm_voice_oohs:2")
-  .mask("<1 0>")
-  .room(4).att(1).rel(1)
-  .lpf(sine.range(100, 3000).fast(16))
-  ._spectrum()
+wt: s("basique").bank("wt_digital").seg(4).n("<0 2 0 ->").scale("d2:minor")
+  .warp("0.4 .5".slow(2))
+  .warpmode("asym")._spectrum()
+
+snare_bass_synth: n(irand(5).seg(8)
+              .rib(142, 2).add("-28")).scale("d1:minor").s("sd:2").ply(16).orbit(2).orbit(4)
+              .velocity(.07).delay(".5:.5:.5")
+  .distort(1)
+  ._pianoroll()
+
+
+nausea: n("<0 - 0 ->").scale("d1:minor").s("supersaw")
+  .att(.1).rel(.2)
+  .lpf(500).lpa(.3).lpe(tri.range(1, 2).slow(4))
+  .lpq(saw.range(6, 12).slow(2))
+  .hpf(200)
+
+// amen: s("breaks:4/2").fit().duck("2:3").duckdepth(.5)
+//   .scrub(irand(16).div(16).seg(8).rib(43, 1))
+//   .delay(".5, .1").gain(.3)
+//   .almostNever(ply("2 | 4"))
+//   ._scope()
+// floor: s("bd!4").duck("4")
+// .delay(".3:.25:.6").velocity(.7)
+
+
+
+ants: n(irand(5).seg(8).rib(142, 2)).scale("d3:minor").s("saw")
+  .delay(".5:.5:.5").hpf(1200).lpf(1700)
+  .clip(.05)
+
+
