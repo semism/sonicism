ce9f14fa1361bfb0de60a3afdfd602670c4fbeb7
_semism_ <128905611+semism@users.noreply.github.com>
2026-06-20 15:05:56 +0200

    Update 00. rerevolved.js

--- strudel/unfinished/00. rerevolved.js
+++ strudel/unfinished/00. rerevolved.js
@@ -10,8 +10,8 @@ line1: n("[0!2] 0 0 [3!2] 4 0 0 4".add("7, 14, 28"))
   .scale("a:minor").s("gm_clavinet").clip(".5 .7").ply(2).pan(saw)
 
 line2: n("[0!2] 0 0 [3!2] 4 0 0 4".add("0, -7"))
-  .scale("a:minor").s("saw, pulse, sine").delay(".2, .5").gain(.7)
-  .lpf(saw.range(200, 300)) //incr
+  .scale("a:minor").s("sine").delay(".2, .5").gain(.7)
+  .lpf(saw.range(300, 300)) //incr
   .lpa(.4).lpenv(4) 
   .room(1).size(4)
   .every("4", x=>x.clip(".5 .7").ply(2))
@@ -19,7 +19,7 @@ line2: n("[0!2] 0 0 [3!2] 4 0 0 4".add("0, -7"))
 top_line: n("[0!2] 0 - 0 2 - 0 2".add("<7 8 9 [[6 5]!4]>").add(7))
   .scale("a:minor").s("sine").echo(4 , 2, .5).room(1).lpf(2000).lpenv(2).lpq(22)
   .pan(sine.slow(32))
-  // .pianoroll()
+  .pianoroll()
 
 
 
