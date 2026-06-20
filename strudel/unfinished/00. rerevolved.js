4125dba436ba83efd3e7a3bae48a8cb2736e8685
_semism_ <128905611+semism@users.noreply.github.com>
2026-06-20 15:19:01 +0200

    Update 00. rerevolved.js

--- strudel/unfinished/00. rerevolved.js
+++ strudel/unfinished/00. rerevolved.js
@@ -6,9 +6,6 @@ amen: s("breaks/2").n("<0@7 1>").fit().gain(2) //reach 3
 .scrub(irand(16).div(16).seg(8).rib(5, 2))
   .almostNever(ply("2 | 4"))
 
-line: n("[0!2] 0 0 [3!2] 4 0 0 4".add("7, 14, 28"))
-  .scale("a:minor").s("gm_clavinet").clip(".5 .7").ply(2).pan(saw)
-
 bass: n("[0!2] 0 0 [3!2] 4 0 0 4".add("0, -7"))
   .scale("a:minor").s("sine").delay(".2, .5").gain(.7)
   .lpa(.4).lpenv(4) 
@@ -20,4 +17,13 @@ top_line: n("[0!2] 0 - 0 2 - 0 2".add("<7 8 9 [[6 5]!4]>"))
   .pan(sine.slow(32)).fast(2)
   .pianoroll()
 
+line: n("[0!2] 0 0 [3!2] 4 0 0 4")
+  .scale("a:minor").s("tri, square, piano").clip(".5 .7").ply(2).pan(saw)
 
+kick: s("bd!4")
+snare: s("sd!2").almostNever(ply(2))
+
+bass2: n("<0!7 2>".add("0, -7")).clip(.3)
+  .scale("a:minor").s("sine")
+  .delay(".2, .5").distort("2:.4")
+  
