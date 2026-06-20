6f15a53b96823a325b529f12cf07eda0898d5d9a
_semism_ <128905611+semism@users.noreply.github.com>
2026-06-20 15:08:27 +0200

    Update 00. rerevolved.js

--- strudel/unfinished/00. rerevolved.js
+++ strudel/unfinished/00. rerevolved.js
@@ -6,10 +6,10 @@ amen: s("breaks/2").n("<0@7 1>").fit().gain(2) //reach 3
 .scrub(irand(16).div(16).seg(8).rib(5, 2))
   .almostNever(ply("2 | 4"))
 
-line1: n("[0!2] 0 0 [3!2] 4 0 0 4".add("7, 14, 28"))
+line: n("[0!2] 0 0 [3!2] 4 0 0 4".add("7, 14, 28"))
   .scale("a:minor").s("gm_clavinet").clip(".5 .7").ply(2).pan(saw)
 
-line2: n("[0!2] 0 0 [3!2] 4 0 0 4".add("0, -7"))
+bass: n("[0!2] 0 0 [3!2] 4 0 0 4".add("0, -7"))
   .scale("a:minor").s("sine").delay(".2, .5").gain(.7)
   .lpa(.4).lpenv(4) 
   .room(1).size(4)
