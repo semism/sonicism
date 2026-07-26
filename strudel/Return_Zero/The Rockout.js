3248d99c85b31551af486839a5aaa0da5d78e0bc
_semism_ <128905611+semism@users.noreply.github.com>
2026-07-26 21:04:54 +0200

    Update The Rockout.js

--- strudel/Return_Zero/The Rockout.js
+++ strudel/Return_Zero/The Rockout.js
@@ -19,25 +19,26 @@ radio: pat.s("piano")
 
 kick: s("bd:2 - [bd!2] -").bank("dr550").room(.2).vel(rand.range(.3, .4))
 hat: s("-!7 oh").bank("dr550").room(.2).delay(.1).vel(rand.range(.13, .35))
-snare: s("- sd - sd").vel(rand.range(.58, .99))
+snare: s("- sd - sd").vel(rand.range(.58, .99)).every(4, ply(2 | 4))
 hh: s("<oh hh>!16")
   .bank("dr550").vel(rand.range(.058, .199)).pan(.2)
 
 rythm_guitar: chord("<Am Cm>").voicing()
   .transpose(-14)
   .s("gm_distortion_guitar")
-  .room(.6).size(5)
+  .room(.6).size(5).delay(.25)
   .lpf(saw.range(2200, 3000)) //remove effect drama
   .ply(16) //ply to strum
   .late(rand.range(0,.0075))
+  .jux(rev) //onoff
 
 var arp = n("<0 3 5 7>*8").scale("<a:minor c:minor>")
 
 const lead_guitar =  arp.s("gm_electric_guitar_jazz").attack(.1)
 
-lead_left: lead_guitar.transpose(-14).pan(0).vel(rand.range(.8,1))
+lead_left: lead_guitar.transpose(-7).pan(0).vel(rand.range(.8,1))
 
-lead_right: lead_guitar.transpose(-7).pan(1).vel(rand.range(.8,1))
+lead_right: lead_guitar.transpose(0).pan(1).vel(rand.range(.8,1))
 
 
 
