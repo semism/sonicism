f9f7384cfb6bb27f084fcf1e6435d05ab8e7ae32
_semism_ <128905611+semism@users.noreply.github.com>
2026-07-26 22:03:51 +0200

    Update The Shoegeezer.js

--- strudel/Return_Zero/The Shoegeezer.js
+++ strudel/Return_Zero/The Shoegeezer.js
@@ -5,7 +5,6 @@ climb = "0"
 const pat = n(irand(12).seg(8).rib("<414 16>", 1).add(climb)).scale("a:minor");
 
 mel: pat.s("gm_guitar_harmonics")
-  .transpose(-14)
   .decay(.4)
   .room(1).size(4)
   .pan(.67)
@@ -14,7 +13,6 @@ mel: pat.s("gm_guitar_harmonics")
 radio: pat.s("piano")
   .decay(.2)
   .ply("<<2 8> 4 2 4>")
-  .transpose(-14)
   .fm("<8 16>".slow(4))
 
 kick: s("bd:2 - [bd!2] -").bank("dr550").room(.2).vel(rand.range(.3, .4))
@@ -30,26 +28,27 @@ var rythm_guitar =
   .s("gm_distortion_guitar")
   .room(2).delay(.25)
 
+support_rythm_guitar: 
+rythm_guitar.transpose("-7")
+  .late(rand.range(.0125,.0075))
+  .pan(perlin.range(0,1).fast(2)).vel(.5)
+
 rythm_guitar: rythm_guitar  
   .lpf(saw.range(2200, 3000)) //remove effect drama
   .late(rand.range(0,.0075))
   .jux(rev) //onoff
   .ply(16) //ply to strum
 
-support_rythm_guitar: 
-rythm_guitar.transpose("-7")
-  .late(rand.range(.0125,.0075))
-  .pan(perlin.range(0,1).fast(2)).vel(.5)
 
 var arp = n("<0 3 5 7>*16").scale("<a2:minor c:minor>")
 
-const lead_guitar =  arp.s("saw").attack(.1).lpf(sine.range(200,600))
+const lead_guitar =  arp.s("saw").attack(.1).decay(.2).distort("3:.1")
 
-lead_left: lead_guitar.transpose(-7).pan(0).vel(rand.range(.8,1)).lpq(10).lpenv(3)
+lead_left: lead_guitar.transpose(-7).pan(0).vel(sine.range(1, .8))
+  .lpf(sine.range(1600,300).slow(4)).lpq(22).lpenv(2)
 
-lead_right: lead_guitar.transpose(0).pan(1).vel(rand.range(.8,1)).lpq(12).lpenv(2)
-
-lead_low: lead_guitar.s("supersaw").transpose(-7)
+lead_right: lead_guitar.pan(1).vel(sine.range(.8, 1))
+  .lpf(sine.range(2200,500).slow(2)).lpq(5).lpenv(3)
 
 bass: pat
   .transpose(-28).s("saw").lpf(300).att(0)
