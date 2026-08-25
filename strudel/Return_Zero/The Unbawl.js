c1140c6b870e0131d631283e29eb0b06f41946cd
_semism_ <128905611+semism@users.noreply.github.com>
2026-08-25 22:34:31 +0200

    Create The Unbawl.js

---
+++ strudel/Return_Zero/The Unbawl.js
@@ -0,0 +1,51 @@
+setCps(120/60/4)
+
+kick: s("sbd bd sbd bd").distort("1:.7").duck("2:3:4:5").duckattack(.2).duckdepth(.8)
+  ._scope()
+snare: s("- sd - sd")
+shak: s("tambourine - tambourine -").delay(.1).vel(rand.range(4, 5))
+
+
+hat: s("[- white]!4").gain(.6)
+  .clip(".34 | .2 ")
+  .every(2, x=>x.ply("2 | 4 "))
+  .orbit(2)
+
+vox: s("numbers:7").scrub(".2 .2 .25 .15")
+  .velocity(rand.range(.7,1))
+  .coarse("<1 2 4 8 16 32 1>")
+
+sub: n(irand(10).seg("4"))
+  .rib("<14 24>", 2)
+  .s("saw")
+  .scale("a1:minor").s("tri, pulse").orbit(5)
+
+bass: n(irand(10).seg("8")).s("saw, gm_voice_oohs")
+  .rib("<14 24>", 2)
+  .scale("a1:minor")
+  .lpf(1000).lpenv(2)
+  .lpq("<12 15>")
+  // .lpq("<22 20>")
+  .jux(rev)
+  .unison(22)
+  .room(.2)
+  .orbit(4)
+
+treb: n(irand(10).seg(4)).s("saw, gm_voice_oohs").gain(.32)
+  .rib("<14 24>", 2)
+  .scale("a3:minor")
+  .late(.25)
+  .delay(.5)
+  .lpf(3500)
+  .orbit(3)
+._pianoroll()
+
+nokia: n(irand(10).seg(8)).s("saw")
+  .rib("<<14 -!3> 24>", 2)
+  .scale("a4:minor")
+  .delay(.5)
+  .hpf(3500)
+  .lpf(7500)
+  .orbit(4)
+
+all(x=>x.whenKey("Control:b", x=>x.coarse("8").color("red").lpenv(5)))
