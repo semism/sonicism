22299ff8191f559c3caf6a08bfc7a441e1ef7ee4
_semism_ <128905611+semism@users.noreply.github.com>
2026-08-29 13:57:38 +0200

    Update The Bachelor.js

--- strudel/Return_Zero/The Bachelor.js
+++ strudel/Return_Zero/The Bachelor.js
@@ -32,7 +32,7 @@ bass: n("0!16").s("supersaw, gm_voice_oohs")
   .orbit(4)
 
 
-bass2: n("0 - 0 0 0 0 0 -").s("supersaw")
+bass2: n("0 - 0 0 1 0 0 -").s("supersaw")
   .scale("<f#:minor!7 a:major>")
   .scaleTrans(-7)
   .lpf(800).lpenv(3).lpq(12)
@@ -47,7 +47,7 @@ s("beqar/8").fit()
 // .scrub("<0 .75 .5 .25>")
 .gain(1.7)
 .room(.2).size(5)
-.orbit(3)
+// .orbit(3)
 
 
 all(x=>x.whenKey("Control:b", x=>x.coarse("8").color("red").lpenv(5)))
